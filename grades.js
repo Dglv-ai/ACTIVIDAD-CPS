21b8f0b7d62dc3f83dee19a5fb1849a94d47403e
David <davidlalo1011@gmail.com>
2026-02-13 05:29:19 -0400

    Agregar funcion redondear

--- grades.js
+++ grades.js
@@ -9,4 +9,12 @@ function evaluarPromedio(promedio){
         return {estado:"Reprobado  😢", clase:"reprobado"};
     }
 
+
+}
+function redondear(valor){
+
+    // Commit #9 - logica redondeo
+    return Math.round(valor*100)/100;
+
+    // Commit #10 - cierre funcion redondear
 }
