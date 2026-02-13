badcc8080d32c2a146869dac1e53dd94506133a7
David <davidlalo1011@gmail.com>
2026-02-13 05:28:10 -0400

    Agregar funcion evaluar promedio

---
+++ grades.js
@@ -0,0 +1,12 @@
+function evaluarPromedio(promedio){
+
+    if(promedio>=51){
+
+        return {estado:"Aprobado 🎉", clase:"aprobado"};
+
+    } else {
+
+        return {estado:"Reprobado 😢", clase:"reprobado"};
+    }
+
+}
